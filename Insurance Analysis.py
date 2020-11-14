5c5b0f5374fc94ec0e2c7dbc4c1e6f92792a0a98
gatlinbredeson <69826930+gatlinbredeson@users.noreply.github.com>
2020-11-13 16:58:24 -0800

    Post-Feedback Analysis Resources
    US Medical Insurance analysis using Python 3 with Pandas, Matplotlib, and SKlearn, update after feedback

--- Insurance Analysis.py
+++ Insurance Analysis.py
@@ -69,16 +69,18 @@ def graph_histograms(dataframe, *columns):
     :return: None
     """
     rows, cols = subplot_size(lst=columns)
-    plt.figure(num='Histograms')
+    plt.figure(num='Histograms', figsize=(rows*4, cols*4))
     for i, col in enumerate(columns):
         plt.subplot(rows, cols, i+1)
         plt.hist(dataframe[col])
-        plt.title(col)
-    plt.subplots_adjust(wspace=0.3, hspace=0.3)
+        plt.title(col.capitalize())
+        plt.xlabel(f'{col.capitalize()} Values')
+        plt.ylabel('Count')
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
     plt.show()
 
 
-def graph_features(dataframe, x_data, y_data, *features_lst):
+def graph_features(dataframe, x_data, y_data, features_list, x_label_str=None, y_label_str=None):
     """
     Makes and shows a figure of subplots. Each graph
             is a scatter plot showing only the data for a subgroup
@@ -87,27 +89,41 @@ def graph_features(dataframe, x_data, y_data, *features_lst):
     :param dataframe: The dataframe containing the X_data, Y_data, and features
     :param x_data: String name of the column of data for the X-axis
     :param y_data: String name of the column of data for the Y-axis
-    :param features_lst: String names of the features to plot. E.g. 'Smoker'
+    :param features_list: List of string names of the features to plot. E.g. ['Smoker', 'children']
+    :param x_label_str: Optional string label for the x-axis data
+    :param y_label_str: Optional string label for the y-axis data
     :return: None
     """
     i = 1
     uniques = 0
-    for feature in features_lst:
+    for feature in features_list:
         uniques += dataframe[feature].nunique()
+    x_min, x_max = 0.9 * dataframe[x_data].min(), 1.1 * dataframe[x_data].max()
+    y_min, y_max = 0.9 * dataframe[y_data].min(), 1.1 * dataframe[y_data].max()
     rows, columns = subplot_size(num=uniques)
     fig = plt.figure(num='Features', figsize=(rows * 4, columns * 4))
     for feature in features_lst:
         featureframe = dataframe[[x_data, y_data, feature]]
         for val in dataframe[feature].unique():
-            plt.subplot(rows, columns, i)
+            fig.add_subplot(rows, columns, i)
             subframe = featureframe[(featureframe[feature] == val)]
             plt.scatter(subframe[x_data], subframe[y_data])
+            plt.axis([x_min, x_max, y_min, y_max])
             if type(val) is str:
                 val = val.capitalize()
-            plt.title(f'{feature.capitalize()}: {val}')
+            plt.title(f'{x_data.capitalize()} vs {y_data.capitalize()} for all '
+                      f'\'{feature.capitalize()} = {val}\'', fontsize=10)
+            if x_label_str:
+                plt.xlabel(x_label_str)
+            else:
+                plt.xlabel(x_data)
+            if y_label_str:
+                plt.ylabel(y_label_str)
+            else:
+                plt.ylabel(y_data)
             i += 1
-    plt.subplots_adjust(wspace=0.3, hspace=0.3)
-    fig.suptitle(f'{x_data.capitalize()} vs {y_data.capitalize()} for Subgroups')
+    plt.subplots_adjust(wspace=0.4, hspace=0.5)
+    fig.suptitle(f'{x_data.capitalize()} vs {y_data.capitalize()} for Specific Feature Values')
     plt.show()
 
 
@@ -155,45 +171,38 @@ df = pd.read_csv('insurance.csv')
 
 # Get a few stats from the data
 print('Stats:')
-get_stats(df, 'bmi')
-get_stats(df, 'age')
-get_stats(df, 'children')
-get_stats(df, 'charges')
+for feature in ['bmi', 'age', 'children', 'charges']:
+    get_stats(df, feature)
 print('')
-proportions(df, 'sex')
-proportions(df, 'smoker')
-proportions(df, 'region')
+for feature in ['sex', 'smoker', 'region']:
+    proportions(df, feature)
 
 # Make histograms to view the shape of the data
 graph_histograms(df, 'bmi', 'age', 'children', 'charges')
-# Mean and standard deviation aren't all that helpful here because
-# the 'charges' data aren't a normal distribution; it's skewed left.
 
 # Look at the data as a whole, age vs. charges
 plt.figure(num='All Data')
 plt.scatter(df.age, df.charges)
 plt.title('All Data: Age vs Charges')
-plt.xlabel('Age')
-plt.ylabel('Charges')
+plt.xlabel('Age (years)')
+plt.ylabel('Charges (USD)')
 plt.show()
-# There are three tiers of charges! Why?
 
 # Lets graph age vs. charges for each unique feature
-graph_features(df, 'age', 'charges', 'sex', 'children', 'smoker', 'region')
-# The 'yes' smokers are all on the middle- and highest-tier charges.
+features_lst = ['sex', 'children', 'smoker', 'region']
+graph_features(df, 'age', 'charges', features_lst, 'Age (years)', 'Charges (USD)')
 
+# Now for just the non-smokers
 nonsmokers = df[(df['smoker'] == 'no')]
-graph_features(nonsmokers, 'age', 'charges', 'sex', 'children', 'smoker', 'region')
-# Graphing only non-smoker data removes the highest-tier charges,
-# so 'yes' smokers account for the highest tier.
-# But the lower- and middle-tier lines still exist among non-smokers.
-# People with 5 kids have lower charges, but the rest of the data
-# looks fairly uniform. Meaning, no other unique feature seems to
-# account for the tiers; all features partake of the two tiers
-# pretty equally. We can't look at the data for any one feature
-# to explain the other two tiers. Is there perhaps a factor
-# affecting the charges which is excluded from our data?
-# Perhaps different tiered plans?
+graph_features(nonsmokers, 'age', 'charges', features_lst, 'Age (years)', 'Charges (USD)')
+
+# Is BMI accounting for it?
+plt.figure('BMI vs Charges')
+plt.scatter(nonsmokers.bmi, nonsmokers.charges)
+plt.title('BMI vs Charges for Non-Smokers')
+plt.xlabel('BMI')
+plt.ylabel('Charges (USD)')
+plt.show()
 
 # Clean the Data for Machine Learning
 # Make dummy variables from the categorical region data
@@ -212,28 +221,12 @@ df['smoker'] = df.smoker.apply(lambda row: 1 if row == 'yes' else 0)
 
 lin_reg(df, 'All Data')
 
-# With an R2 value of around 0.75, we can say that
-# about 75% of the 'charges' data variance is explained
-# by our features (independent variables).
-# There's plenty of room to improve our R2 value;
-# could this be more evidence of a factor missing from
-# our data? Perhaps the tiered medical insurance plans?
-# Perhaps the R2 value was not as high as it could be because
-# I am trying to fit a single trend line to what is basically
-# three lines of data (the three tiers). What if I isolate the
-# lines by selecting ranges of charges? Perhaps coefficients
-# Could be telling in this case. Let's find out.
-
-# Create a new dataframe isolating the tier lines.
-# With (age, charges) being a data point, I used
-# (18, 7500) and (65, 21000) to find a line that separates
-# the tiers. The equation is: y = 287.23x + 2329.79
-# Lets look at only the bottom two tiers by excluding smokers,
-# and separate the lower and middle tiers using this line.
-# Make dummy variables where False is under the line (bottom tier)
-# and True is above the line (middle tier). Then select
-# Dataframes for each tier. Let's also run it separately for
-# the highest tier, the smokers.
+# Create a new dataframe isolating the tiers to analyze each set separately.
+# With (age, charges) being a data point, I used (18, 7500) and (65, 21000)
+# to find a line that separates the tiers. The equation is: y = 287.23x + 2329.79
+# Make dummy variables where False is under the line (bottom tier) and True is
+# above the line (middle tier). Then select Dataframes for each tier.
+# Let's also run it separately for # the highest tier, the smokers.
 
 smokers = df[(df['smoker'] == 1)]
 df['tier'] = df.age.apply(lambda x: 287.23 * x + 2329.79)
@@ -243,25 +236,18 @@ mid_df = df[(df['smoker'] == 0) & (df['tier'] == True)]
 low_df = low_df.drop('tier', axis=1)
 mid_df = mid_df.drop('tier', axis=1)
 
+# Visualize the separated data on a scatter plot
 plt.figure('Tiered Data Separated')
 plt.scatter(low_df.age, low_df.charges, label='Low-Tier Non-Smokers', c='limegreen', alpha=0.4)
 plt.scatter(mid_df.age, mid_df.charges, label='Mid-Tier Non-Smokers', c='forestgreen', marker='v', alpha=0.4)
-# plt.scatter(smokers.age, smokers.charges, label='Smokers')
-#plt.show()
-# Ah! So it's not actually three tiers; it's rather that
-# Smokers and non-smokers each have two tiers.
-
-# I'm updating the scatter plot for the discovery of the two
-# sets of tiers, but I'm leaving the code commented out as
-# a record of how I got there.
 smokers_low = smokers[(smokers['charges'] < 30000)]
 smokers_high = smokers[(smokers['charges'] >= 30000)]
 plt.scatter(smokers_low.age, smokers_low.charges, label='Low-tier Smokers', c='red', alpha=0.4)
 plt.scatter(smokers_high.age, smokers_high.charges, label='High-tier Smokers', c='maroon', marker='v', alpha=0.4)
 plt.legend()
 plt.title('Split Tiers, Age vs Charges for Smokers and Non-Smokers')
-plt.xlabel('Age')
-plt.ylabel('Charges')
+plt.xlabel('Age (years)')
+plt.ylabel('Charges (USD)')
 plt.show()
 
 # Now lets run Regression on each to see if we can find any
@@ -270,16 +256,3 @@ lin_reg(smokers_high, 'High-tier Smoker Data')
 lin_reg(smokers_low, 'Low-tier Smoker Data')
 lin_reg(mid_df, 'High-tier Non-smoker Data')
 lin_reg(low_df, 'Low-tier Non-smoker Data')
-
-# With R2 values greater than 0.9 for the low-tier data, we can
-# say that the features (independent variables) in our data account
-# for 90%+ of the variance in their 'charges' data. But R2 values
-# in the 0.3 - 0.4 range for the high-tier data shows that our
-# features don't sufficiently account for the charges data. Is there
-# indeed some factor or feature we're missing to explain the split in data?
-
-# In any case, being a smoker is a significant factor in the costs.
-# Our low-tier data with good good models show that sex is a
-# fairly significant factor in determining charges. Age is consistent
-# between the two low-tier models, but I'd be curious to find out why the
-# other features aren't as comparable between the two.
